b6d4a764ee436ec45827c7f9b49b2dbc6117c995
gabrielareisvf <gabrielareisvf@gmail.com>
2025-07-08 23:57:22 -0300

    Atualization

--- cypress/e2e/login.js
+++ cypress/e2e/login.js
@@ -1,5 +1,21 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+// login sucesso
+describe('Orange HRM Tests', () => {
+  it('Login - Success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get(".oxd-topbar-header-breadcrumb-module").contains('Dashboard')
+  })
+
+// sem sucesso
+
+it('Login - Fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Test')
+    cy.get("[name='password']").type('Test')
+    cy.get('.oxd-button').click()
+    cy.get('.oxd-alert')
   })
 })
